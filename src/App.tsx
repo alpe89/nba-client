e231c0a9347b6f9991a2bf95d9a60600a26637f4
alpe89 <a.pertusi89@gmail.com>
2020-11-28 01:03:15 +0100

    Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu } from 'antd';
 
-function App() {
+const { Header, Content } = Layout;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header>
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item key="1">Logo</Menu.Item>
+        </Menu>
+      </Header>
+      <Content>Content </Content>
+    </Layout>
   );
 }
 
