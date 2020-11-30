5c1ca168eb874782e5ba8d9bc4acc16f6a6e5b6a
alpe89 <a.pertusi89@gmail.com>
2020-11-30 19:19:58 +0100

    Stato locale e recupero dati da API base

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,28 +1,72 @@
-import React from "react";
-import { Input } from "antd";
+import React, { useState, useEffect } from "react";
+import { Input, Button, Divider } from "antd";
 
-const {Search} = Input;
+import { Player } from "../../types";
 
 const MainContent: React.FC = () => {
+    const [playerName, setPlayerName] = useState<string>("");
+    const [playerSurname, setPlayerSurname] = useState<string>("");
+    const [playerFullName, setPlayerFullName] = useState<string | null>(null);
+    const [playerData, setPlayerData] = useState<Player | null>(null);
+
+    const handlePlayerSearch = (evt: React.FormEvent<HTMLFormElement>): void => {
+        evt.preventDefault();
+        if (playerName === "" && playerSurname === "") {
+            setPlayerFullName(null);
+        } else {
+            setPlayerFullName(playerName + " " + playerSurname);
+        }
+    };
+
+    const resetFullName = (): void => {
+        if (playerName === "" && playerSurname === "") {
+            setPlayerFullName(null);
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`https://www.balldontlie.io/api/v1/players?search=${playerFullName}`);
+            return await response.json();
+        }
+        console.log("full name", playerFullName);
+        if (playerFullName !== null) {
+            fetchData().then(response => setPlayerData(response.data[0]));
+        }
+    }, [playerFullName]);
+
     return (
         <main>
             <section>
-                <form>
+                <form onSubmit={handlePlayerSearch}>
                     <div>
-                        <label>Search for a Player</label>
-                        <Search
-                            placeholder="input search text"
+                        <label>Player Name</label>
+                        <Input
+                            placeholder="Player's name here..."
                             allowClear
-                            enterButton="Search"
                             size="large"
-                            onSearch={() => { console.log('search')} }
+                            value={playerName}
+                            onChange={(evt) => {setPlayerName(evt.target.value); resetFullName();}}
                             />
+                    </div>
+                    <div>
+                        <label>Player Surname</label>
+                        <Input
+                            placeholder="Player's surname here..."
+                            allowClear
+                            size="large"
+                            value={playerSurname}
+                            onChange={(evt) => {setPlayerSurname(evt.target.value); resetFullName();}}
+                            />
+                    </div>
+                    <div>
+                        <Button htmlType="submit" type="primary">Get Player's Card</Button>
                     </div>
                 </form>
             </section>
-            <hr/>
+            <Divider />
             <section>
-                
+                { playerData && <pre>{JSON.stringify(playerData, null, 4)}</pre>}
             </section>
         </main>
     );
---
+++ src/types.d.ts
@@ -0,0 +1,27 @@
+type Player = {
+    first_name: string;
+    height_feet: number | undefined;
+    height_inches: number | undefined;
+    id: number;
+    last_name: string;
+    position: 'PM' | 'G' | 'F' | 'G-F' | 'C' | '';
+    team: Team;
+    weight_pounds: number;
+};
+
+type Team = {
+    abbreviation: string;
+    city: string;
+    conference: 'East' | 'West';
+    division:
+        | 'Atlantic'
+        | 'Pacific'
+        | 'Southeast'
+        | 'Central'
+        | 'Southwest'
+        | 'Northwest';
+    full_name: string;
+    name: string;
+};
+
+export { Player, Team };
